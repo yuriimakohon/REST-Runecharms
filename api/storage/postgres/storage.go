fbc3b1c718604a53392dbdad85e291a5317e849f
Yurii Makohon <yuriimakohon.com>
2021-02-12 18:29:19 +0200

    Added postgresql implementation of storage

---
+++ api/storage/postgres/storage.go
@@ -0,0 +1,141 @@
+package postgres
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4"
+	m "github.com/yuriimakohon/RunecharmsCRUD/api/models"
+	"github.com/yuriimakohon/RunecharmsCRUD/api/storage"
+	"log"
+)
+
+type Storage struct {
+	ctx  context.Context
+	conn *pgx.Conn
+}
+
+func New() *Storage {
+	st := &Storage{ctx: context.Background()}
+	conn, err := pgx.Connect(st.ctx, "postgresql://postgres:secret@localhost:5432/postgres")
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+		return nil
+	}
+
+	if err = conn.Ping(st.ctx); err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	st.conn = conn
+	return st
+}
+
+func (s *Storage) Add(charm m.Charm) (m.Charm, error) {
+	query := `INSERT INTO charms (rune, god, power) VALUES ($1, $2, $3) RETURNING id`
+
+	rows, err := s.conn.Query(s.ctx, query, charm.Rune, charm.God, charm.Power)
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+		return m.Charm{}, err
+	}
+
+	rows.Next()
+	if err = rows.Scan(&charm.Id); err != nil {
+		log.Fatal(err)
+		return m.Charm{}, err
+	}
+
+	return charm, nil
+}
+
+func (s *Storage) Get(id int32) (m.Charm, error) {
+	query := `SELECT * FROM charms WHERE id=$1`
+	charm := m.Charm{}
+
+	rows, err := s.conn.Query(s.ctx, query, id)
+	defer rows.Close()
+	if err != nil {
+		return m.Charm{}, storage.ErrNotFound
+	}
+
+	if !rows.Next() {
+		return m.Charm{}, storage.ErrNotFound
+	}
+
+	if err = rows.Scan(&charm.Id, &charm.Rune, &charm.God, &charm.Power); err != nil {
+		log.Fatal(err)
+		return m.Charm{}, err
+	}
+	return charm, nil
+}
+
+func (s *Storage) GetAll() ([]m.Charm, error) {
+	charms := make([]m.Charm, 0, 1)
+
+	rows, err := s.conn.Query(s.ctx, "SELECT * FROM charms")
+	if err != nil {
+		return []m.Charm{}, err
+	}
+	defer rows.Close()
+
+	charm := m.Charm{}
+	for rows.Next() {
+		if err = rows.Scan(&charm.Id, &charm.Rune, &charm.God, &charm.Power); err != nil {
+			log.Fatal(err)
+		} else {
+			charms = append(charms, charm)
+		}
+	}
+	return charms, nil
+}
+
+func (s *Storage) Delete(id int32) (m.Charm, error) {
+	query := `DELETE FROM charms WHERE id=$1`
+
+	charm, err := s.Get(id)
+	if err != nil {
+		return m.Charm{}, err
+	}
+
+	rows, err := s.conn.Query(s.ctx, query, id)
+	if err != nil {
+		return m.Charm{}, storage.ErrNotFound
+	}
+	rows.Close()
+
+	return charm, nil
+}
+
+func (s *Storage) Update(id int32, charm m.Charm) (m.Charm, error) {
+	query := `UPDATE charms SET rune=$2, god=$3, power=$4 WHERE id=$1`
+
+	if _, err := s.Get(id); err != nil {
+		return m.Charm{}, err
+	}
+
+	rows, err := s.conn.Query(s.ctx, query, id, charm.Rune, charm.God, charm.Power)
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+		return m.Charm{}, err
+	}
+
+	charm.Id = id
+	return charm, nil
+}
+
+func (s *Storage) Len() (int, error) {
+	length := 0
+	rows, err := s.conn.Query(s.ctx, "SELECT COUNT(*) FROM charms")
+	if err != nil {
+		return 0, err
+	}
+
+	rows.Next()
+	defer rows.Close()
+	if err = rows.Scan(&length); err != nil {
+		return 0, err
+	}
+	return length, nil
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/yuriimakohon/RunecharmsCRUD
 go 1.15
 
 require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/mux v1.8.0
+	github.com/jackc/pgx/v4 v4.10.1
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.6
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
