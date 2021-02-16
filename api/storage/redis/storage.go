4e63fba1bd7a9dab1602c54e11118b16e03b029e
Yurii Makohon <yuriimakohon.com>
2021-02-16 19:39:24 +0200

    Replace redis.Client pool with redis.Connect for single connection

--- api/storage/redis/storage.go
+++ api/storage/redis/storage.go
@@ -10,39 +10,39 @@ import (
 )
 
 type Storage struct {
-	ctx context.Context
-	rdb *redis.Client
+	ctx  context.Context
+	conn *redis.Conn
 }
 
 func New() *Storage {
 	st := &Storage{
 		context.Background(),
-		redis.NewClient(&redis.Options{Addr: "localhost:6379"}),
+		redis.NewClient(&redis.Options{Addr: "localhost:6379"}).Conn(context.Background()),
 	}
 
-	isExists, err := st.rdb.Exists(st.ctx, "charm.lastId").Result()
+	isExists, err := st.conn.Exists(st.ctx, "charm.lastId").Result()
 	if err != nil {
 		log.Fatal(err)
 		return nil
 	}
 
 	if isExists == 0 {
-		st.rdb.Set(st.ctx, "charm.lastId", 0, 0)
+		st.conn.Set(st.ctx, "charm.lastId", 0, 0)
 	}
 	return st
 }
 
 func (s *Storage) nextId() int32 {
-	id, _ := s.rdb.Incr(s.ctx, "charm.lastId").Result()
+	id, _ := s.conn.Incr(s.ctx, "charm.lastId").Result()
 	return int32(id)
 }
 
 func (s *Storage) Add(charm m.Charm) (m.Charm, error) {
 	id := s.nextId()
-	s.rdb.Set(s.ctx, sRune(id), charm.Rune, 0)
-	s.rdb.Set(s.ctx, sGod(id), charm.God, 0)
-	s.rdb.Set(s.ctx, sPower(id), charm.Power, 0)
-	s.rdb.SAdd(s.ctx, "idSet", id)
+	s.conn.Set(s.ctx, sRune(id), charm.Rune, 0)
+	s.conn.Set(s.ctx, sGod(id), charm.God, 0)
+	s.conn.Set(s.ctx, sPower(id), charm.Power, 0)
+	s.conn.SAdd(s.ctx, "idSet", id)
 
 	charm.Id = id
 	return charm, nil
@@ -52,15 +52,15 @@ func (s *Storage) Get(id int32) (m.Charm, error) {
 	charm := m.Charm{Id: id}
 	var err error
 
-	if charm.Rune, err = s.rdb.Get(s.ctx, sRune(id)).Result(); err != nil {
+	if charm.Rune, err = s.conn.Get(s.ctx, sRune(id)).Result(); err != nil {
 		return m.Charm{}, storage.ErrNotFound
 	}
 
-	if charm.God, err = s.rdb.Get(s.ctx, sGod(id)).Result(); err != nil {
+	if charm.God, err = s.conn.Get(s.ctx, sGod(id)).Result(); err != nil {
 		return m.Charm{}, storage.ErrNotFound
 	}
 
-	strPower, err := s.rdb.Get(s.ctx, sPower(id)).Result()
+	strPower, err := s.conn.Get(s.ctx, sPower(id)).Result()
 	if err != nil {
 		return m.Charm{}, storage.ErrNotFound
 	}
@@ -77,7 +77,7 @@ func (s *Storage) GetAll() ([]m.Charm, error) {
 	charm := m.Charm{}
 	charms := make([]m.Charm, 0, 1)
 
-	idSet, err := s.rdb.SMembers(s.ctx, "idSet").Result()
+	idSet, err := s.conn.SMembers(s.ctx, "idSet").Result()
 	if err != nil {
 		return []m.Charm{}, err
 	}
@@ -105,10 +105,10 @@ func (s *Storage) Delete(id int32) (m.Charm, error) {
 		return m.Charm{}, storage.ErrNotFound
 	}
 
-	s.rdb.Del(s.ctx, sRune(id))
-	s.rdb.Del(s.ctx, sGod(id))
-	s.rdb.Del(s.ctx, sPower(id))
-	s.rdb.SRem(s.ctx, "idSet", id)
+	s.conn.Del(s.ctx, sRune(id))
+	s.conn.Del(s.ctx, sGod(id))
+	s.conn.Del(s.ctx, sPower(id))
+	s.conn.SRem(s.ctx, "idSet", id)
 
 	return charm, nil
 }
@@ -119,16 +119,16 @@ func (s *Storage) Update(id int32, charm m.Charm) (m.Charm, error) {
 		return m.Charm{}, storage.ErrNotFound
 	}
 
-	s.rdb.Set(s.ctx, sRune(id), charm.Rune, 0)
-	s.rdb.Set(s.ctx, sGod(id), charm.God, 0)
-	s.rdb.Set(s.ctx, sPower(id), charm.Power, 0)
+	s.conn.Set(s.ctx, sRune(id), charm.Rune, 0)
+	s.conn.Set(s.ctx, sGod(id), charm.God, 0)
+	s.conn.Set(s.ctx, sPower(id), charm.Power, 0)
 
 	charm.Id = id
 	return charm, nil
 }
 
 func (s *Storage) Len() (int, error) {
-	length, err := s.rdb.SCard(s.ctx, "idSet").Result()
+	length, err := s.conn.SCard(s.ctx, "idSet").Result()
 	if err != nil {
 		return 0, err
 	}
